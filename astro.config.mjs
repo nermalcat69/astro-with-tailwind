80bde068103ec89152b5d92cc4d1d0a3be9b7aee
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2024-04-22 21:33:23 +0530

    feat: zerops setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+      mode: "standalone"
+    }),
   integrations: [tailwind()]
 });
