179cd92402e7336e97508a0aa47fc9dddff1f0b7
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2024-04-23 21:25:30 +0530

    feat: aaaaaa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
-
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+
+    output: 'server',
   adapter: node({
-      mode: "standalone"
-    }),
+    mode: 'standalone',
+  }),
   integrations: [tailwind()]
 });
